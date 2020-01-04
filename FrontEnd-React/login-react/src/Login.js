dd149b317eda7c14244b6eeb3bdd6885c622d572
Atanu <atanu@Atanus-MacBook-Air.local>
2020-01-05 00:15:26 +0530

    login js added

---
+++ FrontEnd-React/login-react/src/Login.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import ReactDOM from 'react-dom';
+
+class Login extends React.Component {
+    render() {
+      return (
+        <div className="shopping-list">
+          <h1>Shopping List for</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </div>
+      );
+    }
+  }
+
+  export default Login;
