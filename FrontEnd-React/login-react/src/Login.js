d618121bb9eef3b30e0c958f6be128bb050bd165
bhaskarghosh16@icloud.com <bhaskarghosh16@icloud.com>
2020-01-05 23:49:01 +0530

    Added navbar

--- FrontEnd-React/login-react/src/Login.js
+++ FrontEnd-React/login-react/src/Login.js
@@ -1,13 +1,50 @@
-import React, {useState} from "react";
-import ReactDOM from 'react-dom';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomNavbar from './components/Navbar.js'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Alert from 'react-bootstrap/Alert'
+import { properties } from './resources/properties.js'
+
 
 class Login extends React.Component {
     render() {
       return (
-        <div className="login-container">
-         <h3>Login</h3>
+        <div>
+        <CustomNavbar />
+         <h3>{properties.loginCardHeader}</h3>
+         
+      <LoginCard/>
         </div>
       );
+    }
+  }
+  class LoginCard extends React.Component{
+    render(){
+      return (
+        <div>
+        <Row>
+    <Col></Col>
+    <Col><Card border="danger" style={{ width: '29rem' }}>
+   
+    <Card.Body>
+      <Card.Title>
+      <Alert  variant='primary'>
+    Login to continue.
+   </Alert>
+   </Card.Title>
+      <Card.Text>
+        {properties.loginContent}
+      </Card.Text>
+    </Card.Body>
+  </Card></Col>
+    <Col></Col>
+  </Row>
+        
+        </div>
+
+        );
     }
   }
 
---
+++ FrontEnd-React/login-react/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav } from "react-bootstrap";
+import Container from 'react-bootstrap/Container'
+class CustomNavbar extends React.Component {
+    render() {
+      return (
+        <div className="login-container">
+        <Container>
+       <Nav className="justify-content-end" activeKey="/home">
+    <Nav.Item>
+      <Nav.Link href="/home">New User Signup</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-1">Some Page</Nav.Link>
+    </Nav.Item>
+   
+  </Nav>
+</Container>
+</div>
+);
+}
+}
+export default CustomNavbar;
