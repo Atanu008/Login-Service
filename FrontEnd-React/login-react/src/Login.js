08e477ff1358f627dedf30ee27dbc6c05ecf098d
Atanu <atanu@Atanus-MacBook-Air.local>
2020-01-06 21:22:38 +0530

    nav changes

--- FrontEnd-React/login-react/src/Login.js
+++ FrontEnd-React/login-react/src/Login.js
@@ -9,48 +9,43 @@ import { properties } from './resources/properties.js'
 
 
 class Login extends React.Component {
-    render() {
-      return (
-<<<<<<< HEAD
-        <div className="login-container">
-         <h3>Login</h3>
-=======
-        <div>
+  render() {
+    return (
+      <div>
         <CustomNavbar />
-         <h3>{properties.loginCardHeader}</h3>
-         
-      <LoginCard/>
->>>>>>> d618121bb9eef3b30e0c958f6be128bb050bd165
-        </div>
-      );
-    }
+        <h3>{properties.loginCardHeader}</h3>
+
+        <LoginCard />
+      </div>
+    );
   }
-  class LoginCard extends React.Component{
-    render(){
-      return (
-        <div>
+}
+class LoginCard extends React.Component {
+  render() {
+    return (
+      <div>
         <Row>
-    <Col></Col>
-    <Col><Card border="danger" style={{ width: '29rem' }}>
-   
-    <Card.Body>
-      <Card.Title>
-      <Alert  variant='primary'>
-    Login to continue.
+          <Col></Col>
+          <Col><Card border="danger" style={{ width: '29rem' }}>
+
+            <Card.Body>
+              <Card.Title>
+                <Alert variant='primary'>
+                  Login to continue.
    </Alert>
-   </Card.Title>
-      <Card.Text>
-        {properties.loginContent}
-      </Card.Text>
-    </Card.Body>
-  </Card></Col>
-    <Col></Col>
-  </Row>
-        
-        </div>
+              </Card.Title>
+              <Card.Text>
+                {properties.loginContent}
+              </Card.Text>
+            </Card.Body>
+          </Card></Col>
+          <Col></Col>
+        </Row>
 
-        );
-    }
+      </div>
+
+    );
   }
+}
 
-  export default Login;
+export default Login;
--- FrontEnd-React/login-react/src/components/Navbar.js
+++ FrontEnd-React/login-react/src/components/Navbar.js
@@ -3,22 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav } from "react-bootstrap";
 import Container from 'react-bootstrap/Container'
 class CustomNavbar extends React.Component {
-    render() {
-      return (
-        <div className="login-container">
+  render() {
+    return (
+      <div className="login-container">
         <Container>
-       <Nav className="justify-content-end" activeKey="/home">
-    <Nav.Item>
-      <Nav.Link href="/home">New User Signup</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="link-1">Some Page</Nav.Link>
-    </Nav.Item>
-   
-  </Nav>
-</Container>
-</div>
-);
-}
+          <Nav className="justify-content-end" activeKey="/home">
+            <Nav.Item>
+              <Nav.Link href="/signup">New User Signup</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Some Page</Nav.Link>
+            </Nav.Item>
+
+          </Nav>
+        </Container>
+      </div>
+    );
+  }
 }
 export default CustomNavbar;
